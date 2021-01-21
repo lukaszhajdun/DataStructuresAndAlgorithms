b251e1e7967fb67947a301c50032da46ab747968
lukaszhajdun <lukasz.hajdun@gmail.com>
2021-01-21 10:47:49 +0100

    Abstract Data Types - only theory

---
+++ 03-Lists/01-AbstractDataTypes/src/com/lhajdun/Main.java
@@ -0,0 +1,23 @@
+package com.lhajdun;
+
+public class Main {
+
+    /* *
+    *
+    * Abstract Data Type:
+    *
+    *   Doesn't dictate how the data is organized
+    *   Dictates the operations you can perform
+    *   Concrete data structure is usually a concrete class
+    *   Abstract data type is usually an interface
+    *
+    *   Java library has Abstract Data Types such as List, Stack, Queue, Set, Map
+    *   as inbuilt interfaces which are being implemented using various data structures.
+    *   In Java, Abstract Data Types extend the Collections Interface which represents the data type.
+    *
+    * */
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
